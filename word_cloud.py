2a90be49860b6c5fcb28271bfdef520da92de38e
lexical09 <jc61691@gmail.com>
2018-10-19 11:22:42 -0400

    Add files via upload

---
+++ word_cloud.py
@@ -0,0 +1,26 @@
+
+def wordcloud(file_name):
+	import matplotlib.pyplot as plt
+	from wordcloud import WordCloud, STOPWORDS
+
+	text=open (file_name).read()
+
+	# Generate a word cloud image
+	wordcloud = WordCloud().generate(text)
+
+	# Display the generated image:
+	# the matplotlib way:
+	import matplotlib.pyplot as plt
+	plt.imshow(wordcloud, interpolation='bilinear')
+	plt.axis("off")
+
+	# lower max_font_size
+	wordcloud = WordCloud(max_font_size=40).generate(text)
+	plt.figure()
+	plt.imshow(wordcloud, interpolation="bilinear")
+	plt.axis("off")
+	plt.show()
+
+	# The pil way (if you don't have matplotlib)
+	# image = wordcloud.to_image()
+	# image.show()
